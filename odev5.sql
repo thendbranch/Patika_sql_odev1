dda3486f1646c1a141c1b7994b5729136025c35e
thendbranch <barisskarakuss@gmail.com>
2024-03-20 00:32:05 +0300

    odev5

--- odev5.sql
+++ odev5.sql
@@ -14,9 +14,8 @@ ORDER BY length ASC
 OFFSET 5
 LIMIT 5;
 
-/*
-customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
-*/
+
+-- customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
 SELECT * FROM customer
 WHERE store_id = 1
 ORDER BY last_name DESC
